6c8a160a2c78867cb7c2a72f1336767098888448
Chillerbag <ethanwhawkins@gmail.com>
2025-03-15 10:47:29 +1100

    color palette and basic setup. DaisyUI is broken

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+    import { page } from '$app/state'; 
+
+</script>
+
+<header>
+    <div class="logo">Ethan Hawkins </div>
+    <nav>
+        <a href="/" aria-current={page.url.pathname === "/"}>About Me</a>
+        <a href="/projects" aria-current={page.url.pathname === "/projects"}>Projects</a>
+        <a href="/blog" aria-current={page.url.pathname === "/blog"}>Blog</a>
+        <a href="/employment" aria-current={page.url.pathname === "/employment"}>Employment</a>
+    </nav>
+</header>
+  
+<main>
+    <slot><button class="btn w-64 rounded-full">Button</button></slot>
+</main>
