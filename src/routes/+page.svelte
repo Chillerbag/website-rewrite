0325cc5e87572524abfdb9b332610075a6c9d14c
Chillerbag <ethanwhawkins@gmail.com>
2025-03-18 22:02:24 +1100

    styling main page and fixing UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
-
     import { page } from '$app/state'; 
     import { base } from '$app/paths';
+    import { GradientButton, Navbar, NavBrand, NavLi, NavUl, NavHamburger} from 'flowbite-svelte';
+    import { GithubSolid } from 'flowbite-svelte-icons';
 
 </script>
 
 <header>
-    <div class="logo">Ethan Hawkins </div>
-    <nav>
-        <a href="{base}/" class={page.url.pathname === base + "/" || page.url.pathname === base ? 'active' : ''}>About Me</a>
-        <a href="{base}/projects" class={page.url.pathname === base + "/projects" ? 'active' : ''}>Projects</a>
-        <a href="{base}/blog" class={page.url.pathname === base + "/blog" ? 'active' : ''}>Blog</a>
-        <a href="{base}/employment" class={page.url.pathname === base + "/employment" ? 'active' : ''}>Employment</a>
-    </nav>
+    <Navbar color = 'primary'>
+        <NavBrand href="/">
+            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Ethan Hawkins</span>
+        </NavBrand>
+        <NavHamburger  />
+        <NavUl >
+          <NavLi href="{base}/">Home</NavLi>
+          <NavLi href="{base}/projects">Projects</NavLi>
+          <NavLi href="{base}/blog">Blog</NavLi>
+          <NavLi href="{base}/employment">Employment</NavLi>
+        </NavUl>
+    </Navbar>
 </header>
-  
+
 <main>
+    <GradientButton href="https://github.com/Chillerbag" color="greenToBlue">Checkout my Github <GithubSolid class="w-5 h-5 ms-2" /></GradientButton>
 </main>
+
