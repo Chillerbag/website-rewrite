d81249df67f870e87dd15fbaf59c7bc936686d87
Chillerbag <ethanwhawkins@gmail.com>
2025-03-18 20:47:04 +1100

    fixing routes and header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 
     import { page } from '$app/state'; 
+    import { base } from '$app/paths';
 
 </script>
 
 <header>
     <div class="logo">Ethan Hawkins </div>
     <nav>
-        <a href="/" aria-current={page.url.pathname === "/"}>About Me</a>
-        <a href="/projects" aria-current={page.url.pathname === "/projects"}>Projects</a>
-        <a href="/blog" aria-current={page.url.pathname === "/blog"}>Blog</a>
-        <a href="/employment" aria-current={page.url.pathname === "/employment"}>Employment</a>
+        <a href="{base}/" class={page.url.pathname === base + "/" || page.url.pathname === base ? 'active' : ''}>About Me</a>
+        <a href="{base}/projects" class={page.url.pathname === base + "/projects" ? 'active' : ''}>Projects</a>
+        <a href="{base}/blog" class={page.url.pathname === base + "/blog" ? 'active' : ''}>Blog</a>
+        <a href="{base}/employment" class={page.url.pathname === base + "/employment" ? 'active' : ''}>Employment</a>
     </nav>
 </header>
   
 <main>
-    <slot><button class="btn w-64 rounded-full">Button</button></slot>
 </main>
